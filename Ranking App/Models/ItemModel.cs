d7d4e46642b178e9d31c4bf68d0fda39f838d76b
Rashmi Gupta <rashmimittal2497@gmail.com>
2023-01-26 22:01:54 +0530

    Add project files.

---
+++ Ranking App/Models/ItemModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Ranking_App.Models
+{
+    public class ItemModel
+    {
+        public int ItemType { get; set; }
+        public int Id { get; set; }
+        public String Title { get; set; }
+        public int ImageId { get; set; }
+        public int Ranking { get; set; }
+    }
+}
